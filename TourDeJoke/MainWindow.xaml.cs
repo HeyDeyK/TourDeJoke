2e0c2aaae47a40bf8867961020aa4edb9264886f
vileton15 <vileton15@sps-prosek.cz>
2018-02-27 09:00:28 +0100

    kolize

--- TourDeJoke/MainWindow.xaml.cs
+++ TourDeJoke/MainWindow.xaml.cs
@@ -21,16 +21,20 @@ namespace TourDeJoke
     /// </summary>
     public partial class MainWindow : Window
     {
-        double x = 635;
-        double y = 606;
+        double x = 590;
+        double y = 644;
         int mapa = 0;
-        static String[] poleBck = { @"D:\Ondra\Photoshop\export\gamebck.png", @"D:\Ondra\Photoshop\export\gamebck2.png", @"D:\Ondra\Photoshop\export\gamebck3.png" };
+        static String[] poleBck = { @"\\data.sps-prosek.local\vileton15\Stažené\export\export\gamebck.png", @"\\data.sps-prosek.local\vileton15\Stažené\export\export\gamebck2.png", @"\\data.sps-prosek.local\vileton15\Stažené\export\export\gamebck3.png" };
         public MainWindow()
         {
             InitializeComponent();
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += new EventHandler(MovePlayer);
             timer.Start();
+            
+            DispatcherTimer timer2 = new DispatcherTimer();
+            timer2.Tick += new EventHandler(KolizeNPC);
+            timer2.Start();
         }
 
         private void MovePlayer(object sender, EventArgs e)
@@ -44,14 +48,16 @@ namespace TourDeJoke
                     {
                         
                     }
+
                     else
                     {
-                        Console.WriteLine("zmenseni" + mapa);
                         mapa = mapa-1;
+                        KontrolaNPC();
                         CanvasBrush.ImageSource = new BitmapImage(new Uri(poleBck[mapa], UriKind.Relative));
                         x = 1519;
                         Canvas.SetLeft(Player, x);
                     }
+
                     
                 }
                 else
@@ -62,7 +68,7 @@ namespace TourDeJoke
             }
             if (Keyboard.IsKeyDown(Key.Up))
             {
-                if (y <= 377)
+                if (y <= 490)
                 {
                 }
                 else
@@ -74,7 +80,7 @@ namespace TourDeJoke
             }
             if (Keyboard.IsKeyDown(Key.Down))
             {
-                if (y >= 607)
+                if (y >= 705)
                 {
                 }
                 else
@@ -88,13 +94,16 @@ namespace TourDeJoke
             {
                 if (x >= 1520)
                 {
+                    
                     if (mapa ==2)
                     {
 
                     }
                     else
                     {
+                        
                         mapa = mapa+1;
+                        KontrolaNPC();
                         Console.WriteLine("zvetseni"+mapa);
                         CanvasBrush.ImageSource = new BitmapImage(new Uri(poleBck[mapa], UriKind.Relative));
                         x = -160;
@@ -109,6 +118,42 @@ namespace TourDeJoke
                 }
                 
             }
+        }
+        private void KontrolaNPC()
+        {
+            if (mapa == 0)
+            {
+                npc.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                npc.Visibility = Visibility.Hidden;
+                
+            }
+        }
+        private void KolizeNPC(object sender, EventArgs e)
+        {
+            var x1 = Canvas.GetLeft(Player);
+            var y1 = Canvas.GetTop(Player);
+            Rect r1 = new Rect(x1, y1, Player.ActualWidth, Player.ActualHeight);
+
+            var x2 = Canvas.GetLeft(npc);
+            var y2 = Canvas.GetTop(npc);
+            Rect r2 = new Rect(x2, y2, npc.ActualWidth, npc.ActualHeight);
+
+            if (r1.IntersectsWith(r2))
+            {
+
+                mark.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                mark.Visibility = Visibility.Hidden;
+            }
+        }
+        private void NewKolizeNPC(object sender, EventArgs e)
+        {
         }
     }
 }
